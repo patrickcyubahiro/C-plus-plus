6d17f9526da599f6e990fc47e25e9cc11d37def7
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-03-10 23:15:19 -0600

    Create languages.cpp
    A program that prints out the number of languages I use to write code, using arrays.

---
+++ languages.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+
+{
+  string languages[7] = {"C++", "C", "Java", "JavaScript", "PHP", "HTML", "SQL"};
+  
+  for (int i = 0; i < 7; i++) 
+  {
+    cout << languages[i] << "\n";
+  }
+  return 0;
+}
