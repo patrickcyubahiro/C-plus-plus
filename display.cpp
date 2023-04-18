48d637706e2beea3f5a270480637056a8fe68bf3
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-17 23:20:31 -0500

    Create display.cpp
     A C++ program to display a number entered by the End-User.

---
+++ display.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+    int random_Number;
+    
+    cout<<"Enter a number: "<<endl;
+    
+    cin>> random_Number;
+    
+    cout<<"The enterred number is: "<< random_Number;
+     
+    return 0;
+}
