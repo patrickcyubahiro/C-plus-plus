4bbb249511e12f05bd0ffc0867ee7ac49d72b9fe
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-17 21:30:37 -0500

    Create addition.cpp
    A C++ program to calculate the sum of three numbers entered by the end-user.

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+    int first_Number, second_Number, third_Number, sum;
+    
+    cout<<"Enter all the three integers: "<<endl;
+    
+    cin>> first_Number >> second_Number >> third_Number;
+    
+    sum = first_Number + second_Number + third_Number;
+    
+    cout<<"The sum of the enterred three numbers is: "<< sum;
+     
+    return 0;
+}
