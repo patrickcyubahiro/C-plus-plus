9bd2e06e505ec35684d491564be31fff1fb7b4f0
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-19 00:47:45 -0500

    Create SumAndAverage.cpp
    //A C++ Program to calculate sum and average of three numbers.

---
+++ SumAndAverage.cpp
@@ -0,0 +1,40 @@
+//A C++ Program to calculate sum and average of three numbers
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+using namespace std;
+
+int main()
+
+{
+    
+  double firstNumber, secondNumber, thirdNumber, sum, average;
+    
+    cout<< "Enter the first number: " << endl;
+    
+    cin>>firstNumber;
+    
+    cout << "Enter the second number: " << endl;
+    
+    cin>>secondNumber;
+    
+    cout << "Enter the third number: " << endl;
+    
+    cin>>thirdNumber;
+    
+    sum = firstNumber + secondNumber + thirdNumber;
+    
+    average = sum/3;
+    
+    cout << firstNumber << " + " << secondNumber << " + " << thirdNumber << " = " << sum <<endl;
+    
+        cout << " The average of the three entered numbers is: " << average <<endl;
+     
+    return 0;
+}
