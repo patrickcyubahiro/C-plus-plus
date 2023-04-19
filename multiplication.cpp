4ced631ed662b9b2a3e9a8f2211ed78c423a6db4
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-19 00:13:20 -0500

    Create multiplication.cpp
    A C++ program to multiply two floating numbers entered by the end-user.

---
+++ multiplication.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+using namespace std;
+
+int main()
+
+{
+    
+    double firstNumber, secondNumber, multiplication;
+    
+    cout<< "Enter the first number: " << endl;
+    
+    cin>>firstNumber;
+    
+    cout << "Enter the second number: " << endl;
+    
+    cin>>secondNumber;
+    
+    multiplication = firstNumber * secondNumber;
+    
+    cout << firstNumber << " multiplied by " << secondNumber << " is: " << multiplication <<endl;
+
+    return 0;
+}
