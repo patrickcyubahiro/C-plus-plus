3700ab295552376b6ca459d143b10318748200ab
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-19 00:40:36 -0500

    Create  ASCIIValue.cpp
    A C++ program to display the ASCII value of a character.

---
+++ ASCIIValue.cpp
@@ -0,0 +1,26 @@
+//A C++ program to display the ASCII value of a character.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+using namespace std;
+
+int main()
+
+{
+    
+ char c;
+
+     cout << "Enter any Character : ";
+     
+     cin >> c;
+
+     cout << "\nThe ASCII Value of Character [ " << c << " ] is : " << int(c)<<endl;
+     
+    return 0;
+}
