5d376df34e3fdd0ad3d4c56e9307985add8269f2
Patrick Cyubahiro <ampatrickcyubahiro@gmail.com>
2023-04-18 23:55:02 -0500

    Create sizeOfData.cpp
    A C++ program to find the size of the Int, Float, Double, and Char data types.

---
+++ sizeOfData.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+using namespace std;
+
+int main()
+
+{
+    cout << "The size of char data type is: " << sizeof(char) << " byte" << endl;
+    
+    cout << "The size of int data type is: " << sizeof(int) << " bytes" << endl;
+    
+    cout << "The size of float data type is: " << sizeof(float) << " bytes" << endl;
+    
+    cout << "The size of double data type is: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
